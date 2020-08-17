2e27cca457732000b8a21c7e55f51319b7f4e694
jeff <zombie1864@gmail.com>
2020-08-16 21:10:30 -0400

    research phase

---
+++ empty-example/mover.js
@@ -0,0 +1,44 @@
+class Mover {
+    constructor(x, y) {
+      this.pos = createVector(x, y);
+      this.vel = createVector(0, 0);
+      this.acc = createVector(0, 0);
+      this.r = 16;
+    }
+  
+    applyForce(force) {
+      this.acc.add(force);
+    }
+  
+    edges() {
+      if (this.pos.y >= height - this.r) {
+        this.pos.y = height - this.r;
+        this.vel.y *= -1;
+      }
+  
+      if (this.pos.x >= width - this.r) {
+        this.pos.x = width - this.r;
+        this.vel.x *= -1;
+      } else if (this.pos.x <= this.r) {
+        this.pos.x = this.r;
+        this.vel.x *= -1;
+      }
+    }
+  
+    update() {
+      // let mouse = createVector(mouseX, mouseY);
+      // this.acc = p5.Vector.sub(mouse, this.pos);
+      // this.acc.setMag(0.1);
+  
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      this.acc.set(0, 0);
+    }
+  
+    show() {
+      stroke(255);
+      strokeWeight(2);
+      fill(255, 100);
+      ellipse(this.pos.x, this.pos.y, this.r * 2);
+    }
+  }
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,55 @@
+// function setup() {
+//   createCanvas( 400, 400 );
+// }
+
+// function draw() {
+//   background(210,0,200);
+//   let v1 = createVector(40, 50);
+//   let v2 = createVector(40, 50);
+  
+//   ellipse(v1.x, v1.y, 50, 50);
+//   ellipse(v2.x, v2.y, 50, 50);
+//   v1.add(v2);
+//   ellipse(v1.x, v1.y, 50, 50);
+// } 
+
+// let mover;
+
+// function setup() {
+//   createCanvas(400, 400);
+//   mover = new Mover(200, 200);
+// }
+
+// function draw() {
+//   background(0);
+
+//   if (mouseIsPressed) {
+//     let wind = createVector(0.2, 0);
+//     mover.applyForce(wind);
+//   }
+
+//   let gravity = createVector(0, 0.2);
+//   mover.applyForce(gravity);
+
+//   mover.update();
+//   mover.edges();
+//   mover.show();
+// }
+
+var xoff = 0; 
+
 function setup() {
   createCanvas( 400, 400 );
 }
 
 function draw() {
-  background(210,0,200);
-}
+  background(20, 20, 20);
+  // ellipse(200, 200, 24, 24)
+  // let ranX = random(width)
+  // let ranY = random(height)
+  // ellipse(ranX, ranY, 24, 24)
+  // let x = map(noise(100), 0, 1, 0, width)
+  let x = map(noise(xoff), 0, 1, 0, width)
+  xoff += 0.02;
+  ellipse(x, 200, 24, 24)
+}  
