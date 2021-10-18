c0dabbe1f434dec9e33cdbcba6c7d8c63f3938b2
zombie1864 <zombie1864@gmail.com>
2021-10-18 15:48:37 -0400

    fixed navBar css

--- title.js
+++
@@ -1,9 +0,0 @@
-// const title = document.getElementById("#title path")
-// // const title = document.querySelectorAll("#title path")
-// // const title = document.querySelectorAll
-
-// for (let i = 0; i < title.length; i++) {
-//     // debugger
-//     console.log(`Letter ${i} is ${title[i].getTotalLength()}`);
-//     // console.log('TODAY YES');
-// }
